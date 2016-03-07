031e9b8e3a270695c8c797052dae1f1061694ee3
Adnan Yaqoob Virk <adnanyaqoobvirk@gmail.com>
2016-03-07 14:13:16 +0500

    lunatic_python library path fix

--- vlc-subliminal-plugin.lua
+++ vlc-subliminal-plugin.lua
@@ -1,3 +1,5 @@
+url = require "net.url"
+
 function descriptor()
   return { 
     title = "Download Subtitles",
@@ -12,19 +14,19 @@ end
 
 function activate()
   vlc.msg.dbg("[VSP] Welcome")
-  
-  assert(package.loadlib('./lunatic_python.so', 'luaopen_python'))()
+
+  local current_directory = debug.getinfo(1).source:match("@(.*)/.*lua$") 
+  assert(package.loadlib(current_directory .. '/lunatic_python.so', 'luaopen_python'))()
 
   if vlc.input.item() then
-    url = require "net.url"
-    video_path = url.parse(vlc.input.item():uri()).path
+    local video_path = url.parse(vlc.input.item():uri()).path
     python.execute("import os")
     python.execute("from babelfish import Language")
     python.execute("from subliminal import download_best_subtitles, save_subtitles, scan_video")
     python.execute("videos = [scan_video('" .. video_path .. "')]")
     python.execute("subtitles = download_best_subtitles(videos, {Language('eng')})")
     python.execute("save_subtitles(videos[0], subtitles[videos[0]])")
-    subtitle_path = python.eval("os.path.splitext(videos[0].name)[0] + '.en.srt'")
+    local subtitle_path = python.eval("os.path.splitext(videos[0].name)[0] + '.en.srt'")
     vlc.input.add_subtitle(subtitle_path)
     vlc.osd.message("Subtitle Downloaded and Added.", 1234, "top-right", 1000000)
     vlc.msg.dbg("[VSP] Subtitle download successful.")
